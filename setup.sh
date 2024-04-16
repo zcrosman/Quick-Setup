3f4cc524d88086ee668d70e5b199b2f7553e4da3
zcrosman <zcrosman@users.noreply.github.com>
2024-04-16 07:57:28 -0500

    subl fix

--- setup.sh
+++ setup.sh
@@ -142,8 +142,8 @@ install_BOFs() {
 
 fast () {
     #Submime
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null 
-    echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list 
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null 
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list 
     sudo apt-get update 
     sudo apt-get -y install sublime-text 
 
